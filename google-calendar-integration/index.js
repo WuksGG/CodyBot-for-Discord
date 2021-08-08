e45f7b78a53a8b14a71573e7d4187c92a420eaae
Michael Chan <mhcchan@ucdavis.edu>
2021-08-07 22:44:42 -0700

    User Prompt + Confirmation

--- google-calendar-integration/index.js
+++ google-calendar-integration/index.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
+const readline = require('readline');
 require('dotenv').config({ path: './configuration/.env' });
 
 const { calendar } = require('#config');
 const { pgp, db } = require('#helpers/database');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const storeToDatabase = async ({ items: events }) => {
   const eventsColumnSet = new pgp.helpers.ColumnSet(
@@ -16,7 +22,6 @@ const storeToDatabase = async ({ items: events }) => {
     ],
     { table: 'events' },
   );
-
   const parsedCalendarData = events
     .filter((calendarEvent) => calendarEvent.start.dateTime)
     .map((calendarEvent) => {
@@ -37,15 +42,14 @@ const storeToDatabase = async ({ items: events }) => {
         location,
       };
     });
-
   const query = pgp.helpers.insert(parsedCalendarData, eventsColumnSet);
-
   try {
     await db.none(query);
     process.stdout.write(`${parsedCalendarData.length} events added to the database!\n`);
+    return 1;
   } catch (e) {
-    process.stdout.write(e.stack);
-    process.stdout.write('\n');
+    process.stdout.write(`${e.stack}\n`);
+    return 0;
   }
 };
 
@@ -64,17 +68,40 @@ const getGoogleCalendarData = async () => {
     });
     return [null, data];
   } catch (e) {
-    process.stdout.write(e.stack);
-    process.stdout.write('\n');
+    process.stdout.write(`${e.stack}\n`);
     return [e];
   }
 };
 
-const populateDatabase = async () => {
-  const [err, data] = await getGoogleCalendarData();
-  if (err) return err;
-  storeToDatabase(data);
-  return 0;
+const createTableIfNotExists = () => {
+  const query = '';
 };
 
-populateDatabase();
+const populateDatabase = async () => {
+  // Create table if does not exist
+
+  // Clear table if exists
+
+  // Populate table
+  const [err, data] = await getGoogleCalendarData();
+  if (err) return err;
+  return storeToDatabase(data);
+};
+
+const writeNewLines = () => {
+  process.stdout.write('\n\n');
+};
+process.on('exit', writeNewLines);
+
+rl.question('\x1b[31m\x1b[1mRunning this script will delete any existing \'events\' table in the database.\x1b[0m\n'
+  + '\x1b[36mAre you sure you want to run this script?\x1b[0m (y/N) ', async (answer) => {
+  process.removeListener('exit', writeNewLines);
+  process.stdout.write('\n');
+  if (['yes', 'y'].includes(answer.toLowerCase())) {
+    const status = await populateDatabase();
+    process.stdout.write('\n');
+    process.exit(status);
+  }
+  // Non-yes answer, closing process without execution
+  process.exit(0);
+});
