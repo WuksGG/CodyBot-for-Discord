57fc260e9c12875be0e0553f0861e930338542f2
Michael Chan <mhcchan@ucdavis.edu>
2021-08-08 01:02:34 -0700

    More comment cleanup

--- google-calendar-integration/oauth2.js
+++ google-calendar-integration/oauth2.js
@@ -19,18 +19,12 @@ const opn = require('open');
 const destroyer = require('server-destroy');
 const { google } = require('googleapis');
 
-/**
- * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
- */
 const keyPath = path.join(__dirname, '..', 'configuration', 'oauth2.json');
 let keys = { redirect_uris: [''] };
 if (fs.existsSync(keyPath)) {
   keys = require(keyPath).web;
 }
 
-/**
- * Create a new OAuth2 client with the configured keys.
- */
 const oauth2Client = new google.auth.OAuth2(
   keys.client_id,
   keys.client_secret,
@@ -41,7 +35,6 @@ google.options({ auth: oauth2Client });
 
 async function authenticate(scopes) {
   return new Promise((resolve, reject) => {
-    // grab the url that will be used for authorization
     const authorizeUrl = oauth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: scopes.join(' '),
@@ -55,7 +48,7 @@ async function authenticate(scopes) {
             res.end('Authentication successful! Please return to the console.');
             server.destroy();
             const { tokens } = await oauth2Client.getToken(qs.get('code'));
-            oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
+            oauth2Client.credentials = tokens;
             resolve(oauth2Client);
           }
         } catch (e) {
@@ -63,7 +56,6 @@ async function authenticate(scopes) {
         }
       })
       .listen(3000, () => {
-        // open the browser to the authorize url to start the workflow
         opn(authorizeUrl, { wait: false }).then((cp) => cp.unref());
       });
     destroyer(server);
