e2080f2fd586e2e715d555d069e46afe90a216fa
Michael Chan <mhcchan@ucdavis.edu>
2021-08-08 22:16:55 -0700

    Working on messageCreate init

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const calendar = require('./calendar');
 const calendarData = require('./calendarData');
+const messageCreate = require('./messageCreate');
 
 module.exports = {
   calendar,
   calendarData,
+  messageCreate,
 };
---
+++ models/messageCreate/index.js
@@ -0,0 +1,6 @@
+// const welcome = require('./welcome');
+const init = require('./init');
+
+module.exports = {
+  init,
+};
